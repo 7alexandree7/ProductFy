20e8ecf337d04f358ba2b7cf68ce5c8cc3b4e891
alexandre <alexandre020602@gmail.com>
2026-01-04 19:45:17 -0300

    helper route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, Backend!");
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to productfy API - Powered by Postgres, Express, Drizzle, and Clerk Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments"
+        },
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
