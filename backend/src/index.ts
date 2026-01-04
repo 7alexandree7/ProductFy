eb387e832c5dd6e9c6ce5d06a0b2b7e0d7bb7a39
alexandre <alexandre020602@gmail.com>
2026-01-04 20:41:24 -0300

    changing the order of the middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { ENV } from './config/env';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(clerkMiddleware());
+
 
 
 app.get('/', (req, res) => {
