de7d1746e3eec18a4b5c707562eaf9881b819481
alexandre <alexandre020602@gmail.com>
2026-01-04 20:13:06 -0300

    add type

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,7 +1,13 @@
 import "dotenv/config";
 
-export const ENV = {
+type EnvConfig = {
+    PORT: number | string;
+    NODE_ENV: 'development' | 'production' | 'test';
+    DB_URL: string | undefined;
+}
+
+export const ENV: EnvConfig = {
     PORT: process.env.PORT || 3000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: (process.env.NODE_ENV as 'development' | 'production' | 'test') || 'development',
     DB_URL: process.env.DB_URL
 }
