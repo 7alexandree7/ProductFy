a10fb1ccf04f90c310e1474c24616a36b3772628
alexandre <alexandre020602@gmail.com>
2026-01-05 10:05:19 -0300

    applyng cors

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,10 +4,16 @@ type EnvConfig = {
     PORT: number | string;
     NODE_ENV: 'development' | 'production' | 'test';
     DB_URL: string | undefined;
+    FRONTEND_URL: string | undefined;
+    CLERK_PUBLISHABLE_KEY?: string;
+    CLERK_SECRET_KEY?: string;
 }
 
 export const ENV: EnvConfig = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: (process.env.NODE_ENV as 'development' | 'production' | 'test') || 'development',
-    DB_URL: process.env.DB_URL
+    DB_URL: process.env.DB_URL,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 }
