5a61a9855a8300a0a451280de792ab91695a1833
alexandre <alexandre020602@gmail.com>
2026-01-07 03:21:17 -0300

    Add product controller and routes for retrieving all products and product by ID

---
+++ backend/src/controllers/productController.ts
@@ -0,0 +1,29 @@
+
+import type { Request, Response } from "express";
+import * as queries from "../db/queries";
+import { getAuth } from "@clerk/express";
+
+export const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await queries.getAllProducts();
+        return res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error / products not found" });
+    }
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const filteredProduct = await queries.getProductById(id);
+
+        if (!filteredProduct) return res.status(404).json(`product with id ${id} not found`);
+
+        res.status(200).json(filteredProduct);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error / product not found" });
+    }
+
+}
--- backend/src/routes/ProductsRoutes.ts
+++ backend/src/routes/ProductsRoutes.ts
@@ -1,5 +1,10 @@
 import { Router} from "express";
+import { requireAuth } from "@clerk/express"; 
+import * as productController from "../controllers/productController"
 
 const router = Router();
+
+router.get("/", productController.getAllProducts)
+router.get("/:id", productController.getProductById)
 
 export default router
