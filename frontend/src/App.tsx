fa5fb3bbd6ac23a3c77c3a75b749c06c7dad6e84
alexandre <alexandre020602@gmail.com>
2026-01-09 19:56:49 -0300

    basic routes config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router"
+import NavBar from "./components/NavBar/NavBar"
+import HomePage from "./pages/HomePage/HomePage"
+import ProductPage from "./pages/ProductPage/ProductPage"
+import ProfilePage from "./pages/ProfilePage/ProfilePage"
+import CreatePage from "./pages/CreatePage/CreatePage"
+import EditProductPage from "./pages/EditProductPage/EditProductPage"
 
 
 function App() {
-  
+
   return (
-    <div>
-      <button className="btn btn-primary">FrontEnd</button>
+    <div className="min-h-screen bg-base-100">
+      <NavBar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="create" element={<CreatePage />} />
+          <Route path="/edit/:id" element={<EditProductPage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
